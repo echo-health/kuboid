673a58027d17bfae6a910a7060474499db619e28
Ryan McGrath <ryan.mcgrath@echo.co.uk>
2017-05-26 16:50:09 +0200

    Added check for required CLI tooling
    When you run kuboid, if any of gcloud or kubectl are missing then it
    will install them first.
    
    If the gcloud tooling is too old, it will update all the components
    first before running any commands.

--- bin/kuboid.js
+++ bin/kuboid.js
@@ -3,10 +3,13 @@
 const program = require('commander');
 const packageJson = require('../package');
 
-program
+require('../lib/dependencies')
+.check()
+.then(() => program
   .version(packageJson.version)
   .command('config', 'operate on deployment config')
   .command('switch', 'switch kubernetes cluster')
   .command('deploy', 'deploy application')
   .command('generate', 'generate kubenetes config via templates')
-  .parse(process.argv);
+  .parse(process.argv)
+);
